ed8b2cb9a907f5c8cee3b04034d5824da5cb2afa
Hieu Nguyen <pirackr.inbox@gmail.com>
2025-08-27 11:07:51 -0700

    fix(common): clean up unused comments

--- modules/common.nix
+++ modules/common.nix
@@ -68,15 +68,6 @@
     EDITOR = "vim";
   };
 
-
-  # programs.emacs = {
-  #   enable = true;
-  #   package = pkgs.emacs;  # replace with pkgs.emacs-gtk, or a version provided by the community overlay if desired.
-  #   extraConfig = ''
-  #     (setq standard-indent 2)
-  #   '';
-  # };
-
   # Enable direnv for automatic environment loading
   programs.direnv = {
     enable = true;
