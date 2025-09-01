8e2f126950a5cae9087fbbf9384c9effc1c7edc8
Pirackr <pirackr.inbox@gmail.com>
2025-09-01 12:29:33 -0700

    refactor(emacs): clean up flags - we don't need it anymore

--- modules/emacs/evil.nix
+++ modules/emacs/evil.nix
@@ -1,73 +1,71 @@
 { config, lib, pkgs, ... }:
 
 {
-  config = lib.mkIf (config.modules.emacs.enable && config.modules.emacs.evil) {
-    programs.emacs.init.usePackage = {
-      evil = {
-        enable = true;
-        package = epkgs: epkgs.evil;
-        hook = [ "(org-mode . (lambda () evil-org-mode))" ];
-        init = ''
-          (setq evil-undo-system 'undo-fu)
-          (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
-          (setq evil-want-keybinding nil)
-        '';
-        config = ''
-          (evil-mode 1)
-        '';
-      };
+  programs.emacs.init.usePackage = {
+    evil = {
+      enable = true;
+      package = epkgs: epkgs.evil;
+      hook = [ "(org-mode . (lambda () evil-org-mode))" ];
+      init = ''
+        (setq evil-undo-system 'undo-fu)
+        (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+        (setq evil-want-keybinding nil)
+      '';
+      config = ''
+        (evil-mode 1)
+      '';
+    };
 
-      evil-leader = {
-        enable = true;
-        package = epkgs: epkgs.evil-leader;
-        after = [ "evil" ];
-        config = ''
-          (global-evil-leader-mode t)
-          (evil-leader/set-leader "<SPC>")
-          (evil-leader/set-key
-            "a" 'org-agenda
-            "p f" 'projectile-find-file
-            "p p" 'projectile-switch-project
-            "p s" 'consult-ripgrep
-            "d x w" 'delete-trailing-whitespace)
-        '';
-      };
+    evil-leader = {
+      enable = true;
+      package = epkgs: epkgs.evil-leader;
+      after = [ "evil" ];
+      config = ''
+        (global-evil-leader-mode t)
+        (evil-leader/set-leader "<SPC>")
+        (evil-leader/set-key
+          "a" 'org-agenda
+          "p f" 'projectile-find-file
+          "p p" 'projectile-switch-project
+          "p s" 'consult-ripgrep
+          "d x w" 'delete-trailing-whitespace)
+      '';
+    };
 
-      evil-surround = {
-        enable = true;
-        package = epkgs: epkgs.evil-surround;
-        after = [ "evil" ];
-        config = ''
-          (global-evil-surround-mode 1)
-        '';
-      };
+    evil-surround = {
+      enable = true;
+      package = epkgs: epkgs.evil-surround;
+      after = [ "evil" ];
+      config = ''
+        (global-evil-surround-mode 1)
+      '';
+    };
 
-      evil-collection = {
-        enable = true;
-        package = epkgs: epkgs.evil-collection;
-        after = [ "evil" ];
-        config = ''
-          (when (require 'evil-collection nil t)
-            (evil-collection-init))
-        '';
-      };
+    evil-collection = {
+      enable = true;
+      package = epkgs: epkgs.evil-collection;
+      after = [ "evil" ];
+      config = ''
+        (when (require 'evil-collection nil t)
+          (evil-collection-init))
+      '';
+    };
 
-      treemacs-evil = {
-        enable = true;
-        package = epkgs: epkgs.treemacs-evil;
-        after = [ "treemacs" "evil" ];
-      };
+    treemacs-evil = {
+      enable = true;
+      package = epkgs: epkgs.treemacs-evil;
+      after = [ "treemacs" "evil" ];
+    };
 
-      evil-org = {
-        enable = true;
-        package = epkgs: epkgs.evil-org;
-        after = [ "org" "evil" ];
-        hook = [ "(org-mode . evil-org-mode)" ];
-        config = ''
-          (require 'evil-org-agenda)
-          (evil-org-agenda-set-keys)
-        '';
-      };
+    evil-org = {
+      enable = true;
+      package = epkgs: epkgs.evil-org;
+      after = [ "org" "evil" ];
+      hook = [ "(org-mode . evil-org-mode)" ];
+      config = ''
+        (require 'evil-org-agenda)
+        (evil-org-agenda-set-keys)
+      '';
     };
   };
 }
