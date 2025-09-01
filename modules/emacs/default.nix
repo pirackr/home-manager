d1c82ac39561fc9d9c834b9efe0fe39a5cf231ed
Pirackr <pirackr.inbox@gmail.com>
2025-08-31 22:27:35 -0700

    feat(emacs): try splitting emacs to modules

--- modules/emacs/default.nix
+++ modules/emacs/default.nix
@@ -3,10 +3,17 @@
 {
   imports = [
     ./emacs-init.nix
+    ./evil.nix
   ];
 
   options.modules.emacs = {
     enable = lib.mkEnableOption "Emacs configuration";
+
+    evil = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable Evil mode configuration for Emacs";
+    };
   };
 
   config = lib.mkIf config.modules.emacs.enable {
@@ -366,61 +373,6 @@
             '';
           };
 
-          evil = {
-            enable = true;
-            package = epkgs: epkgs.evil;
-            hook = [ "(org-mode . (lambda () evil-org-mode))" ];
-            init = ''
-              (setq evil-undo-system 'undo-fu)
-              (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
-              (setq evil-want-keybinding nil)
-            '';
-            config = ''
-              (evil-mode 1)
-            '';
-          };
-
-          evil-leader = {
-            enable = true;
-            package = epkgs: epkgs.evil-leader;
-            after = [ "evil" ];
-            config = ''
-              (global-evil-leader-mode t)
-              (evil-leader/set-leader "<SPC>")
-              (evil-leader/set-key
-                "a" 'org-agenda
-                "p f" 'projectile-find-file
-                "p p" 'projectile-switch-project
-                "p s" 'consult-ripgrep
-                "d x w" 'delete-trailing-whitespace)
-            '';
-          };
-
-          evil-surround = {
-            enable = true;
-            package = epkgs: epkgs.evil-surround;
-            after = [ "evil" ];
-            config = ''
-              (global-evil-surround-mode 1)
-            '';
-          };
-
-          evil-collection = {
-            enable = true;
-            package = epkgs: epkgs.evil-collection;
-            after = [ "evil" ];
-            config = ''
-              (when (require 'evil-collection nil t)
-                (evil-collection-init))
-            '';
-          };
-
-          treemacs-evil = {
-            enable = true;
-            package = epkgs: epkgs.treemacs-evil;
-            after = [ "treemacs" "evil" ];
-          };
-
           tree-sitter = {
             enable = true;
             package = epkgs: epkgs.tree-sitter;
@@ -537,17 +489,6 @@
             package = epkgs: epkgs.org-bullets;
             after = [ "org" ];
             hook = [ "(org-mode . org-bullets-mode)" ];
-          };
-
-          evil-org = {
-            enable = true;
-            package = epkgs: epkgs.evil-org;
-            after = [ "org" "evil" ];
-            hook = [ "(org-mode . evil-org-mode)" ];
-            config = ''
-              (require 'evil-org-agenda)
-              (evil-org-agenda-set-keys)
-            '';
           };
 
           # Additional packages for global configuration
