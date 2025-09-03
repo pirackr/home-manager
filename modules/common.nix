20de719fd35517a1d9791ca30ad1b1d4bc0346ba
Pirackr <pirackr.inbox@gmail.com>
2025-09-02 22:53:53 -0700

    fix(common): clean up spaces + add more packages

--- modules/common.nix
+++ modules/common.nix
@@ -23,12 +23,13 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    pkgs.neofetch 
+    pkgs.neofetch
     pkgs.nix-prefetch-github
     pkgs.curl
     pkgs.tmux
     pkgs.less
-    
+    pkgs.ripgrep
+
     pkgs.noto-fonts
     pkgs.nerd-fonts.fira-code
   ];
@@ -86,57 +87,57 @@
       # Basic settings
       confirm_os_window_close = 0;
       dynamic_background_opacity = true;
-      
+
       # Font settings
       disable_ligatures = "never";
-      
+
       # Window settings
       remember_window_size = false;
       initial_window_width = 500;
       initial_window_height = 300;
-      
+
       # Start in fullscreen mode
       startup_session = "none";
       hide_window_decorations = "titlebar-only";
       # Note: kitty doesn't have a direct fullscreen option, but we can simulate it
       # You can press Cmd+Ctrl+F (macOS) or F11 (Linux) to toggle fullscreen after startup
-      
+
       # Tab settings
       tab_bar_edge = "bottom";
       tab_bar_style = "powerline";
-      
+
       # Catppuccin Frappe theme
       background_opacity = "0.95";
-      
+
       # Catppuccin Frappe colors
       foreground = "#c6d0f5";
       background = "#303446";
       selection_foreground = "#303446";
       selection_background = "#f2d5cf";
-      
+
       # Cursor colors
       cursor = "#f2d5cf";
       cursor_text_color = "#303446";
-      
+
       # URL underline color when hovering with mouse
       url_color = "#f2d5cf";
-      
+
       # Kitty window border colors
       active_border_color = "#babbf1";
       inactive_border_color = "#737994";
       bell_border_color = "#e5c890";
-      
+
       # OS Window titlebar colors
       wayland_titlebar_color = "system";
       macos_titlebar_color = "system";
-      
+
       # Tab bar colors
       active_tab_foreground = "#232634";
       active_tab_background = "#ca9ee6";
       inactive_tab_foreground = "#c6d0f5";
       inactive_tab_background = "#292c3c";
       tab_bar_background = "#232634";
-      
+
       # Colors for marks (marked text in the terminal)
       mark1_foreground = "#303446";
       mark1_background = "#babbf1";
@@ -144,37 +145,37 @@
       mark2_background = "#ca9ee6";
       mark3_foreground = "#303446";
       mark3_background = "#85c1dc";
-      
+
       # The 16 terminal colors
-      
+
       # black
       color0 = "#51576d";
       color8 = "#626880";
-      
+
       # red
       color1 = "#e78284";
       color9 = "#e78284";
-      
+
       # green
       color2 = "#a6d189";
       color10 = "#a6d189";
-      
+
       # yellow
       color3 = "#e5c890";
       color11 = "#e5c890";
-      
+
       # blue
       color4 = "#8caaee";
       color12 = "#8caaee";
-      
+
       # magenta
       color5 = "#f4b8e4";
       color13 = "#f4b8e4";
-      
+
       # cyan
       color6 = "#81c8be";
       color14 = "#81c8be";
-      
+
       # white
       color7 = "#b5bfe2";
       color15 = "#a5adce";
