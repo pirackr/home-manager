94a9ec31b8ef6dddecbc830b64a4cf2f4e0bd265
Pirackr <pirackr.inbox@gmail.com>
2025-09-08 16:09:46 -0700

    fix(common): make tmux term to be kitty

--- modules/common.nix
+++ modules/common.nix
@@ -18,7 +18,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.stateVersion = "24.11"; # Please rea the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -95,14 +95,14 @@
   # Configure tmux
   programs.tmux = {
     enable = true;
-    prefix = "C-a";  # Change prefix from C-b to C-a
+    prefix = "C-b";  
     baseIndex = 1;   # Start window numbering at 1
     escapeTime = 0;  # No delay for escape key press
     keyMode = "vi";  # Use vi-style key bindings
     mouse = true;    # Enable mouse support
     
     # Terminal settings
-    terminal = "screen-256color";
+    terminal = "xterm-kitty";
     
     # Additional configuration
     extraConfig = ''
